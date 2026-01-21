0251a36a0c57710fe190dacfb0608b09d6e70986
poleagui06 <jeanpoleagui06@gmail.com>
2026-01-21 12:44:48 -0500

    ejercicio paquete 02 version 2

--- EjemplosExcepciones/src/paquete02/Ejercicio.java
+++ EjemplosExcepciones/src/paquete02/Ejercicio.java
@@ -4,24 +4,31 @@
  */
 package paquete02;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Pole
  */
 public class Ejercicio {
+
+    static Scanner entrada = new Scanner(System.in);
+
     public static void main(String[] args) {
         int valor1 = ingresarValor();
         int valor2 = ingresarValor();
-        
-        int suma = obtenerSuma(valor1, valor2)
+
+        int suma = obtenerSuma(valor1, valor2);
+        imprimir(valor1, valor2, suma);
     }
+
     public static int ingresarValor() {
-          boolean bandera = true;
-        int valor1;
+        boolean bandera = true;
+        int valor1 = 0;
         while (bandera) {
             try {
 
-                System.out.println("Ingrese el valor 1");
+                System.out.println("Ingrese el valor");
                 valor1 = entrada.nextInt();
                 if (valor1 < 0) {
                     throw new Exception("Número negativos");
@@ -30,16 +37,26 @@ public class Ejercicio {
                     throw new Exception("Número impar");
                 }
                 bandera = false;
-            } catc
-        
+
+            } catch (Exception e) {
+                System.out.printf("Valor no permitido\n"
+                        + "Error : %s\n", e);
+            }
+        }
+
+        return valor1;
     }
-    public static void imprimir() {
-        
-    }
-    public static int obtenerSuma() {
-        
+
+   
+    public static int obtenerSuma(int a, int b) {
+        int suma = a + b;
+        return suma;
+
     }
     
-    
-    
+    public static void imprimir(int a, int b, int c) {
+        System.out.printf("La suma de %s + %s = %s\n", a, b, c);
+
+    }
+
 }
